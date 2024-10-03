9f9a6417ea4c3b04494416d2d3157d011972eee1
tiru-2001 <92062663+tiru-2001@users.noreply.github.com>
2024-10-03 16:23:56 +0530

    solved bug in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://cngen.netlify.app',
-  'https://cngenfrontend.vercel.app'
+  'https://cngenfrontend.vercel.app',
  'https://www.cngengroup.com'
 ];
 
